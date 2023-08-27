37c8a78c93a2fe587b2916d35ba33e0efcba31e0
Jonathan David Lee <111817527+JonathanDLee24@users.noreply.github.com>
2023-08-26 22:26:46 -0700

    Update deviceonline.sh
    Added code to make a purple led,
    
    also 4th version that uses the full output for use with multiple case commands to iterate over for multiple LED and device online use.

--- deviceonline.sh
+++ deviceonline.sh
@@ -57,3 +57,75 @@ else
   gpioctl -f /dev/gpioc2 7 duty 0 >/dev/null
   gpioctl -f /dev/gpioc2 8 duty 0 >/dev/null
 fi
+
+Version with purple LED setting multiple colors on for same led
+
+#!/bin/sh
+pfctl -vvss | grep ', rule 79' >/dev/null
+res=$?
+if [ $res = 0 ]; 
+then
+  sysctl -q dev.gpio.2.led.1.pwm=1
+  gpioctl -f /dev/gpioc2 3 duty 100 >/dev/null
+  sysctl -q dev.gpio.2.led.2.pwm=1
+  gpioctl -f /dev/gpioc2 7 duty 0 >/dev/null
+  gpioctl -f /dev/gpioc2 6 duty 100 >/dev/null
+  gpioctl -f /dev/gpioc2 8 duty 100 >/dev/null
+else
+  sysctl -q dev.gpio.2.led.1.pwm=1
+  gpioctl -f /dev/gpioc2 3 duty 0 >/dev/null
+  sysctl -q dev.gpio.2.led.2.pwm=1
+  gpioctl -f /dev/gpioc2 6 duty 0 >/dev/null
+  gpioctl -f /dev/gpioc2 8 duty 0 >/dev/null
+  gpioctl -f /dev/gpioc2 7 duty 7 >/dev/null
+fi
+
+
+
+version with full variable to iterate over for multiple items variable is named state
+
+#!/bin/sh
+state=$( pfctl -vvss )
+res=1
+resb=1
+case "$state" in 
+  *, rule 79*)
+    res=0
+    ;;
+esac
+case "$state" in 
+  *192.168.1.11*)
+    resb=0
+    ;;
+esac
+if [ $res = 0 ] && [ $resb = 0 ]; 
+then
+  sysctl -q dev.gpio.2.led.1.pwm=1
+  gpioctl -f /dev/gpioc2 3 duty 50 >/dev/null
+  sysctl -q dev.gpio.2.led.2.pwm=1
+  gpioctl -f /dev/gpioc2 7 duty 0 >/dev/null
+  gpioctl -f /dev/gpioc2 6 duty 50 >/dev/null
+elif [ $res = 0 ];
+then
+  sysctl -q dev.gpio.2.led.1.pwm=1
+  gpioctl -f /dev/gpioc2 3 duty 0 >/dev/null
+  sysctl -q dev.gpio.2.led.2.pwm=1
+  gpioctl -f /dev/gpioc2 7 duty 0 >/dev/null
+  gpioctl -f /dev/gpioc2 6 duty 50 >/dev/null
+elif [ $resb = 0 ];
+then
+  sysctl -q dev.gpio.2.led.2.pwm=1
+  gpioctl -f /dev/gpioc2 7 duty 0 >/dev/null
+  gpioctl -f /dev/gpioc2 6 duty 0 >/dev/null
+  sysctl -q dev.gpio.2.led.1.pwm=1
+  gpioctl -f /dev/gpioc2 3 duty 50 >/dev/null
+else
+  sysctl -q dev.gpio.2.led.1.pwm=1
+  gpioctl -f /dev/gpioc2 3 duty 0 >/dev/null
+  sysctl -q dev.gpio.2.led.2.pwm=1
+  gpioctl -f /dev/gpioc2 6 duty 0 >/dev/null
+  gpioctl -f /dev/gpioc2 7 duty 50 >/dev/null
+  
+fi
+
+
